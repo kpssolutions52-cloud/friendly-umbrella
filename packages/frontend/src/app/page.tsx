8d33826a21e04b9aacf53f83888f411dd66d9190
sanka <priyanthaw520@gmail.com>
2025-12-18 15:22:53 +0530

    fix: Pass event parameter to handleSearch in mobile search modal

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1079,7 +1079,7 @@ export default function Home() {
                   <X className="w-5 h-5 text-gray-500" />
                 </button>
               </div>
-              <form onSubmit={(e) => { e.preventDefault(); handleSearch(); setShowMobileSearchModal(false); }} className="relative">
+              <form onSubmit={(e) => { handleSearch(e); setShowMobileSearchModal(false); }} className="relative">
                 <SearchIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
                 <Input
                   type="text"
