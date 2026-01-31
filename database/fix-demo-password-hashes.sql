103fc6be5c0544196a62c6ce49956ae884f82eee
sanka <priyanthaw520@gmail.com>
2026-01-31 23:14:17 +0800

    Fix demo account password hashes with correct bcrypt hashes that match passwords

--- database/fix-demo-password-hashes.sql
+++ database/fix-demo-password-hashes.sql
@@ -2,15 +2,15 @@
 -- This script updates the password hashes and trims any whitespace
 
 -- Update QS Demo User Password (DemoQS123!)
--- Hash: $2a$10$PocqDPcRsoyxbTw6bHWJPufiF.SI.eEEs9kAymNRF4K5.WfQA6VIK
+-- Hash: $2a$10$CY0uahKHmqnV5k8c4sfSAOhXjrNHT0CEx8.dcTqs2m0GA/kj6.qp6
 UPDATE users
-SET password_hash = TRIM('$2a$10$PocqDPcRsoyxbTw6bHWJPufiF.SI.eEEs9kAymNRF4K5.WfQA6VIK')
+SET password_hash = '$2a$10$CY0uahKHmqnV5k8c4sfSAOhXjrNHT0CEx8.dcTqs2m0GA/kj6.qp6'
 WHERE email = 'demo.qs@constructionguru.com';
 
 -- Update Supplier Demo User Password (DemoSupplier123!)
--- Hash: $2a$10$zE3zSl896jCRFPVDoMw2fOzW36wHdR2VF0W4ZzmG9G931SJKk.z6q
+-- Hash: $2a$10$B26rNgjBZlpFfnj9ej/U3eZeOANMsCPYNMVGNeY6ZeAAuFg48szEa
 UPDATE users
-SET password_hash = TRIM('$2a$10$zE3zSl896jCRFPVDoMw2fOzW36wHdR2VF0W4ZzmG9G931SJKk.z6q')
+SET password_hash = '$2a$10$B26rNgjBZlpFfnj9ej/U3eZeOANMsCPYNMVGNeY6ZeAAuFg48szEa'
 WHERE email = 'demo.supplier@constructionguru.com';
 
 -- Verify the updates
